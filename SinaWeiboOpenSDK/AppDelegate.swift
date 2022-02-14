8992ac88d815b057cd6f11a726ab023aa68cb9e2
liujun <1035841713@qq.com>
2022-02-14 17:38:15 +0800

    first commit

---
+++ SinaWeiboOpenSDK/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SinaWeiboOpenSDK
+//
+//  Created by jun on 2022/02/14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
